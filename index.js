1eb9e780bbba33e8f274266219dfb93a18ac0b8d
viniciushrk <viniciusrosa444@gmail.com>
2021-01-06 12:21:02 -0400

    first

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const ActiveDirectory = require('activedirectory');
+// const { json } = require('express');
+
+const app = express()
+const porta = 3333;
+app.use(express.json())
+
+const config = {
+    url: 'ldap://rondonia.local',
+    baseDN: 'dc=domain,dc=com',
+};
+const ad = new ActiveDirectory(config);
+
+app.get( '/',(req, res) => {
+
+    console.log('teste')
+    const username = '94360065272'
+    const password = 'Alfa*3264'
+    
+    ad.authenticate(username, password, function(err, auth) {
+        if (err) {
+            console.log('ERROR: '+JSON.stringify(err));
+            return res.send(err);
+        }
+        if (auth) {
+            console.log('Authenticated!');
+            return res.send('Authenticated!')
+        }
+        else {
+            console.log('Authentication failed!');
+            return res.send('Authentication failed!!')
+        }
+    });
+})
+
+app.listen(porta, () => {
+    console.log(`Example app listening at http://localhost:${porta}`)})
